3d750b1e39a779860ee2e6656afd740d74f5eccd
jbpraxxys <jbbernardo.praxxys@gmail.com>
2025-01-22 10:15:50 +0800

    feat: homepage

--- src/components/general/TheFooter.vue
+++ src/components/general/TheFooter.vue
@@ -1,3 +1,57 @@
 <template>
-    <footer>Test Footer</footer>
+    <footer class="bg-black w-full relative text-white">
+        <img src="/images/overlay.png" alt="overlay" class="absolute top-0 left-0 w-full h-full" />
+        <div class="max-w-[1440px] m-auto px-10">
+            <div
+                class="w-full relative z-[1] flex justify-between border-b border-neutral-500 py-16"
+            >
+                <div class="space-y-2.5">
+                    <img src="/images/logo.png" alt="logo" class="w-20" />
+                    <div class="space-y-2.5">
+                        <p>Address: 3 Green St, UK, W1K 6RN, GB</p>
+                        <p>Contact: +123 4567 890</p>
+                        <p>Email Address: alpha@example.com</p>
+                    </div>
+                </div>
+
+                <div class="flex space-x-10">
+                    <div>
+                        <p class="font-bold mb-2.5">Information</p>
+                        <div class="space-y-2.5 flex flex-col">
+                            <a href="#" class="text-sm">Promo Events</a>
+                            <a href="#" class="text-sm">Contact Us</a>
+                            <a href="#" class="text-sm">About Us</a>
+                            <a href="#" class="text-sm">Models</a>
+                            <a href="#" class="text-sm">Blogs</a>
+                        </div>
+                    </div>
+                    <div>
+                        <p class="font-bold mb-2.5">Legal</p>
+                        <div class="space-y-2.5 flex flex-col">
+                            <a href="#" class="text-sm">Privacy Policy</a>
+                            <a href="#" class="text-sm">Terms and Conditions</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex space-x-5">
+                    <a href="">
+                        <img src="/images/facebook.svg" alt="facebook" />
+                    </a>
+                    <a href="">
+                        <img src="/images/instagram.svg" alt="instagram" />
+                    </a>
+                    <a href="">
+                        <img src="/images/twitter.svg" alt="twitter" />
+                    </a>
+                    <a href="">
+                        <img src="/images/yt.svg" alt="youtube" />
+                    </a>
+                </div>
+            </div>
+            <div class="text-center py-5 text-xs">
+                <p>© 2025. All Right Reserved. Alpha Aesthetic</p>
+            </div>
+        </div>
+    </footer>
 </template>
---
+++ src/components/slider/ProjectSlider.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const modules = [Autoplay, Pagination];
+
+const props = defineProps({
+    items: {
+        type: Object,
+    },
+});
+</script>
+<template>
+    <section>
+        <div class="max-w-[1440px] m-auto px-10 py-16">
+            <div class="max-w-[531px] mb-10">
+                <h2 class="text-2xl font-serif mb-6">Note Worthy Projects</h2>
+                <p>
+                    Explore our standout projects that showcase the talent, versatility, and
+                    professionalism of our models.
+                </p>
+            </div>
+            <div>
+                <swiper
+                    :slides-per-view="4"
+                    :space-between="50"
+                    :loop="true"
+                    :modules="modules"
+                    :autoplay="{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }"
+                >
+                    <swiper-slide v-for="(item, index) in items" :key="index">
+                        <div
+                            class="aspect-w-[350] aspect-h-[440] rounded-lg overflow-hidden bg-black"
+                        >
+                            <img class="w-full h-full object-cover" :src="item.image" alt="image" />
+                        </div>
+                    </swiper-slide>
+                </swiper>
+            </div>
+        </div>
+    </section>
+</template>
